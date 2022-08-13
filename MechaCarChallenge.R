8c1797328ac066e0e39bdfcba87a86b4ba0d715c
Melissa Tan <tan.melissagrace@gmail.com>
2022-08-12 19:33:18 -0600

    added deliverable 2 r script

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,5 +1,7 @@
 library(dplyr)
 
+##Deliverable 1
+
 #load in data
 mecha_car <- read.csv("MechaCar_mpg.csv")
 
@@ -8,3 +10,14 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mecha_car))
 
+
+##Deliverable 2
+
+#load in data
+suspension_coil <- read.csv("Suspension_Coil.csv")
+
+#create dataframe with mean, median, variance, and standard deviation
+total_summary <- suspension_coil %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI)) 
+
+#create dataframe to group manufacturing lot by the mean, median, variance, and standard deviation of PSI colu,m
+lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
